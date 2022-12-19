c241c7e1de42fade66d66fac35b166373110837c
Heithem Tarchouna <75651342+HeithemTarchouna@users.noreply.github.com>
2022-12-19 13:32:49 +0100

    redefined the ensures_tables

--- cons.pl
+++ cons.pl
@@ -48,27 +48,31 @@ schedule(AllReservations) :-
   % For all pairs of reservations, check if they overlap in time
   % and, if they do, ensure that they are assigned to different tables
   forall(combination(2, AllReservations, [Reservation1, Reservation2]),
-         ( Reservation1 = [_, _, TimeInMinutes1, ExpectedEnd1, _, _, TableNumber1],
-           Reservation2 = [_, _, TimeInMinutes2, ExpectedEnd2, _, _, TableNumber2],
-           ( overlap(TimeInMinutes1, ExpectedEnd1, TimeInMinutes2, ExpectedEnd2)
-             -> TableNumber1 #\= TableNumber2
-             ;  true
-           )
-         )
-         
-         
-         
+        ensure_tables(AllReservations,[Reservation1,Reservation2])
          ).
 
 
-         combination(0, _, []).
-        combination(N, [X|Xs], [X|Ys]) :-
-          N > 0,
-          N1 is N - 1,
-          combination(N1, Xs, Ys).
-        combination(N, [_|Xs], Ys) :-
-          N > 0,
-          combination(N, Xs, Ys).
+% Ensure that all reservations that overlap in terms of time are not assigned to the same table during that peroid
+ensure_tables(AllReservations, [Reservation1, Reservation2]) :-
+    Reservation1 = [_, _, TimeInMinutes1, ExpectedEnd1, _, _, TableNumber1],
+    Reservation2 = [_, _, TimeInMinutes2, ExpectedEnd2, _, _, TableNumber2],
+    overlap(TimeInMinutes1, ExpectedEnd1, TimeInMinutes2, ExpectedEnd2),
+    TableNumber1 #\= TableNumber2.
+% if two tables don't overlap, then they can be assigned to the same table
+ensure_tables(AllReservations, [Reservation1, Reservation2]) :-
+      Reservation1 = [_, _, TimeInMinutes1, ExpectedEnd1, _, _, TableNumber1],
+      Reservation2 = [_, _, TimeInMinutes2, ExpectedEnd2, _, _, TableNumber2],
+      \+overlap(TimeInMinutes1, ExpectedEnd1, TimeInMinutes2, ExpectedEnd2).
+
+
+combination(0, _, []).
+combination(N, [X|Xs], [X|Ys]) :-
+  N > 0,
+  N1 is N - 1,
+  combination(N1, Xs, Ys).
+  combination(N, [_|Xs], Ys) :-
+  N > 0,
+  combination(N, Xs, Ys).
 
 test(Reservations) :-
   schedule(Reservations).
