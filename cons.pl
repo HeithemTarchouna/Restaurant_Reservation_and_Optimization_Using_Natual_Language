ef2ef578adbc2c75a82389160581252d570db337
Heithem Tarchouna <75651342+HeithemTarchouna@users.noreply.github.com>
2022-12-20 20:27:49 +0100

    workingCons

--- cons.pl
+++ cons.pl
@@ -77,12 +77,20 @@ combination(N, [X|Xs], [X|Ys]) :-
 
 
 
-test(Reservations,MaxList) :-
-  getOptimal(Reservations,MaxList), 
-  schedule(MaxList).
-  %labeling(max(Sum),[Sum]). % the cut is used to stop the search after the first optimal is found, if you backtrack you get the second best and so on.
-
-
+    test(Reservations, MaxList) :-
+      subseq0(Reservations, MaxList),
+      maplist(reservation, MaxList),
+      schedule(MaxList),
+      total_seats(MaxList, TotalSeats),
+      findall(Var, member([_, _, _, _, _, _, Var], MaxList), Vars),
+      labeling([max(TotalSeats)], Vars).
+  
+  total_seats(Reservations, TotalSeats) :-
+    maplist(number_of_seats, Reservations, Seats),
+    sumlist(Seats, TotalSeats).
+  
+  number_of_seats(Reservation, NumberOfSeats) :-
+    Reservation = [_, _, _, _, _, NumberOfSeats, _].
 
 % subseq0(List, Subsets): true if Subsets is a subset of List
 %https://stackoverflow.com/questions/4912869/subsets-in-prolog
@@ -94,44 +102,3 @@ subseq1([_|Tail], Rest) :-
    subseq0(Tail, Rest).
 subseq1([Head|Tail], [Head|Rest]) :-
    subseq1(Tail, Rest).
-
-% sum(A single reservation of a list of reservations, Sum): Sum is the sum of seats filled in the reservation or reservations
-% this first part handles the case of single reservation
-sum_seats_filled([], 0).
-sum_seats_filled([_, _, _, _, _, NumberOfPeople, _], NumberOfPeople).
-sum_seats_filled([[_, _, _, _, _, NumberOfPeople, _]|T], Sum) :-
-      sum_seats_filled(T, Sum1),
-      Sum #= Sum1 + NumberOfPeople.
-      sum_seats_filled([[_, _, _, _, _, NumberOfPeople, _]], NumberOfPeople).
-% this second part handles the case of multiple reservations
-sum_seats_filled([[[_, _, _, _, _, NumberOfPeople, _]]|T], Sum) :-
-      sum_seats_filled(T, Sum1),
-      write(Sum1),
-    Sum #= Sum1 + NumberOfPeople.
-
-% getOptimal(Reservations, MaxList): true if MaxList is the list of reservations that maximizes the number of seats filled
-getOptimal(Reservations,MaxList):-
-      %subseq0(Reservations, Subsets),
-      find_max_sum(Reservations, MaxList).
-
-% Find the maximum sum in a list of lists
-find_max_sum(Lists, MaxList) :-
-    %create a list of sums of seats filled in each list (possible subset of reservations)
-    findall(Subsets, subseq0(Lists,Subsets), AllSublists),
-    delete(AllSublists, [], Sublists),
-    maplist(sum_seats_filled, Sublists, Sums),
-    bubble_sort(Sums, SortedSums),
-    %Gives you back the maximum sum in the list , if you backtrack it gives the seconds largest sum etc
-    nth1(Index, Sums, MaxSum),
-    %find the list of reservations that corresponds to that sum
-    nth1(Index, Sublists, MaxList).
-
-
-%http://kti.mff.cuni.cz/~bartak/prolog/sorting.html
-bubble_sort(List,Sorted):-b_sort(List,[],Sorted).
-b_sort([],Acc,Acc).
-b_sort([H|T],Acc,Sorted):-bubble(H,T,NT,Max),b_sort(NT,[Max|Acc],Sorted).
-   
-bubble(X,[],[],X).
-bubble(X,[Y|T],[Y|NT],Max):-X<Y,bubble(X,T,NT,Max).
-bubble(X,[Y|T],[X|NT],Max):-X>=Y,bubble(Y,T,NT,Max).
